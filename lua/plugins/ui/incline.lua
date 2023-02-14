c14fb18ad792c7d660240ce487aac10bca959599
Jordi <jcalafat97@gmail.com>
2023-02-14 11:23:36 +0100

    misc

--- lua/plugins/ui/incline.lua
+++
@@ -1,5 +0,0 @@
-return {
-  'b0o/incline.nvim',
-  event = 'BufReadPre',
-  config = true,
-}
--- lua/plugins/ui/misc.lua
+++ lua/plugins/ui/misc.lua
@@ -2,4 +2,5 @@ return {
   { 'kyazdani42/nvim-web-devicons', lazy = true },
   { 'NvChad/nvim-colorizer.lua', lazy = true, config = true },
   { 'MunifTanjim/nui.nvim', lazy = true },
+  { 'b0o/incline.nvim', event = 'BufReadPre', config = true },
 }
